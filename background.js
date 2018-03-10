f0904205a69c3e9d6d695da0c2e34081926dc68b
Matteo Nastasi <nastasi@alternativeoutput.it>
2018-03-10 14:05:25 +0100

    first implementation of a couple of methods to communicate with content.js and with background.js

--- background.js
+++ background.js
@@ -1,6 +1,14 @@
 console.log("background.js: begin");
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     console.log("inside message !: [" + message.msg + "]");
-    sendResponse({farewell: "goodbye"});
+    sendResponse({'reply': message.msg});
 });
+
+chrome.runtime.onMessageExternal.addListener(
+  function(request, sender, sendResponse) {
+      console.log("background.js: received msg: " + request.msg);
+      sendResponse({'reply': request.msg});
+  });
+
+
 console.log("background.js: end");
